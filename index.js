d887c5899f8f68f6667c55899d9b48ce00bc4332
wley3337@gmail.com <wley3337@gmail.com>
2018-03-29 18:16:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ var katzDeliLine = [];
 var i = 1;
 //Build a function that a new customer will use when entering the deli. The function, takeANumber, should accept the current line of people, katzDeliLine, along with the new person's name as parameters. The function should return their position in line. And don't go being too programmer-y and give them their index. These are normal people. If they are 7th in line, tell them that. Don't get their hopes up by telling them they are number 6 in line.
 
-function takeANumber (katzDeli){
-  
-  katzDeli.push(i);
-  i++;
+function takeANumber (){
+  katzDeliLine.push(i);
+  i = i++;
   return `Welcome, ${newName}. You are number ${katzDeli.length} in line.`;
 }
 
